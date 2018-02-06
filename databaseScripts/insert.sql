00731a05495784eaf8f16298c976ee1eaa5e45bc
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2018-02-06 20:49:16 +0100

    Update insert.sql

--- databaseScripts/insert.sql
+++ databaseScripts/insert.sql
@@ -1,17 +1,17 @@
 /* Langues fr/en/cn */
-INSERT INTO `langue`(`id`, `nom`) VALUES (DEFAULT, 'fr');
-INSERT INTO `langue`(`id`, `nom`) VALUES (DEFAULT, 'en');
-INSERT INTO `langue`(`id`, `nom`) VALUES (DEFAULT, 'cn');
+INSERT INTO langue VALUES (DEFAULT, 'fr');
+INSERT INTO langue VALUES (DEFAULT, 'en');
+INSERT INTO langue VALUES (DEFAULT, 'cn');
 
 /* Concepts */
-INSERT INTO `concept`(`id`, `nom`) VALUES (DEFAULT, 'Siege');
-INSERT INTO `concept`(`id`, `nom`) VALUES (DEFAULT, 'Siege sans bras');
+INSERT INTO concept VALUES (DEFAULT, 'Siege');
+INSERT INTO concept VALUES (DEFAULT, 'Siege sans bras');
 
 /* Termes */
-INSERT INTO `terme`(`id`, `motCle`, `langue_id`, `concept_id`) VALUES (DEFAULT, 'siege', '1', '1');
-INSERT INTO `terme`(`id`, `motCle`, `langue_id`, `concept_id`) VALUES (DEFAULT, 'seat', '2', '1');
-INSERT INTO `terme`(`id`, `motCle`, `langue_id`, `concept_id`) VALUES (DEFAULT, '椅子', '3', '1');
+INSERT INTO terme VALUES (DEFAULT, 'siege', '1', '1');
+INSERT INTO terme VALUES (DEFAULT, 'seat', '2', '1');
+INSERT INTO terme VALUES (DEFAULT, '椅子', '3', '1');
 
-INSERT INTO `terme`(`id`, `motCle`, `langue_id`, `concept_id`) VALUES (DEFAULT, 'banc', '1', '2');
-INSERT INTO `terme`(`id`, `motCle`, `langue_id`, `concept_id`) VALUES (DEFAULT, 'bench', '2', '2');
-INSERT INTO `terme`(`id`, `motCle`, `langue_id`, `concept_id`) VALUES (DEFAULT, '长凳', '3', '2');
+INSERT INTO terme VALUES (DEFAULT, 'banc', '1', '2');
+INSERT INTO terme VALUES (DEFAULT, 'bench', '2', '2');
+INSERT INTO terme VALUES (DEFAULT, '长凳', '3', '2');
