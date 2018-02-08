458525b9cda33012c5a069376124f996fd8e9025
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2018-02-08 22:28:02 +0100

    affichage du titre d'un article

--- views/article/tableShowById.php
+++ views/article/tableShowById.php
@@ -1,11 +1,13 @@
 <?php
 	if($data['article']->type == 'pdf'){
 		echo('
+		<h1 class="display-3">'.$data['article']->nom.'<a style="font-size:50%" href="./'.$data['article']->chemin.'" target="_blank">&rarr;</a></h1>
 		<object data="'.$data['article']->chemin.'" type="application/pdf">
 			<p>Ce navigateur ne permet pas de visualiser les pdf: <a href="'.$data['article']->chemin.'">Télécharger le pdf</a>.</p>
 		</object>'
 		);
 	}else{
+		echo '<h1 class="display-3">'.$data['article']->nom.'<a style="font-size:50%" href="./'.$data['article']->chemin.'" target="_blank">&rarr;</a></h1>';
 		include($data['article']->chemin);
 	}
 ?>
