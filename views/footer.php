af9a35f1d6776d9808169d31c846d5fabafea8f8
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2018-02-09 16:27:09 +0100

    ajout contenu footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,11 @@
 	</div>
-		<footer style="text-align:center;">
-			Icon made by <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a> from www.flaticon.com 
-		</footer>
 	</body>
+		<footer>
+			<p><image src="./images/favicon-big.png"><span style="vertical-align:middle">BTG</span></p>
+			<p>
+				BTG est un projet réalisé dans le cadre de la L3 Informatique.<br/>
+				Le code complet est disponible sur GitHub à <a href="https://github.com/Alexandre-Moreau/projet-s6">cette adresse</a>.
+			</p>
+			<p>Icon made by <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a> from www.flaticon.com</p>
+		</footer>
 	</html>
