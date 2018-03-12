46ebbfcf34e54381c613679a52a07f84a55f5554
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2018-03-12 21:36:35 +0100

    bouton pour ajouter un fichier

--- views/article/tableShowAll.php
+++ views/article/tableShowAll.php
@@ -1,26 +1,10 @@
 <?php
-/*
-	if($data['articles'] != []){
-		echo('
-		<table class="tableAffichage">
-			<tr><th>Nom</th><th>Type</th></tr>'
-		);
-		foreach($data['articles'] as $article){
-			echo '<tr><td><a href=".?r=article/showById&id='.$article->id.'">'.$article->nom.'</a></td><td><a href=".?r=article/showById&id='.$article->id.'">'.$article->type.'</a></td></tr>';
-		}
-		echo('</table>');
-	}else{
-		echo '<p>Aucun article</p>';
-		echo '<a href=".?r=article/create"><button class="btn btn-outline-success my-2 my-sm-0">Ajouter un article</button></a>';
-	}*/
-	if($data['articles'] != []){
-		foreach($data['articles'] as $article){
+	foreach($data['articles'] as $article){
 			echo '<div class="fileIconDiv">';
 			echo '<a href=".?r=article/showById&id='.$article->id.'"><image class="fileIcon" src="./images/file-'.$article->type.'.svg">'.$article->nom.'</a>';
 			echo '</div>';
 		}
-	}else{
-		echo '<p>Aucun article</p>';
-		echo '<a href=".?r=article/create"><button class="btn btn-outline-success my-2 my-sm-0">Ajouter un article</button></a>';
-	}
 ?>
+<div class="fileIconDiv">
+	<a href=".?r=article/create"><image class="fileIcon" src="./images/file-add.svg"></a>
+</div>
