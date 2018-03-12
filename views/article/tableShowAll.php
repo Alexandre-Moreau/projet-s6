bfeb3e8e93df6800e43fba86a2f223cf3a1242a4
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2018-03-12 21:28:45 +0100

    nouvel affichage d'article

--- views/article/tableShowAll.php
+++ views/article/tableShowAll.php
@@ -1,4 +1,5 @@
 <?php
+/*
 	if($data['articles'] != []){
 		echo('
 		<table class="tableAffichage">
@@ -8,6 +9,16 @@
 			echo '<tr><td><a href=".?r=article/showById&id='.$article->id.'">'.$article->nom.'</a></td><td><a href=".?r=article/showById&id='.$article->id.'">'.$article->type.'</a></td></tr>';
 		}
 		echo('</table>');
+	}else{
+		echo '<p>Aucun article</p>';
+		echo '<a href=".?r=article/create"><button class="btn btn-outline-success my-2 my-sm-0">Ajouter un article</button></a>';
+	}*/
+	if($data['articles'] != []){
+		foreach($data['articles'] as $article){
+			echo '<div class="fileIconDiv">';
+			echo '<a href=".?r=article/showById&id='.$article->id.'"><image class="fileIcon" src="./images/file-'.$article->type.'.svg">'.$article->nom.'</a>';
+			echo '</div>';
+		}
 	}else{
 		echo '<p>Aucun article</p>';
 		echo '<a href=".?r=article/create"><button class="btn btn-outline-success my-2 my-sm-0">Ajouter un article</button></a>';
--- views/footer.php
+++ views/footer.php
@@ -6,6 +6,9 @@
 				BTG est un projet réalisé dans le cadre de la L3 Informatique.<br/>
 				Le code complet est disponible sur GitHub à <a href="https://github.com/Alexandre-Moreau/projet-s6">cette adresse</a>.
 			</p>
-			<p>Icon made by <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a> from www.flaticon.com</p>
+			<p>
+				Icon made by <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a> from www.flaticon.com<br/>
+				Vector images by <a href="https://www.flaticon.com/authors/smashicons" target="_blank">Smashicons</a> from www.flaticon.com
+			</p>
 		</footer>
 	</html>
