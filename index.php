e126eb888eb6e3004f903df8be4b41785b995406
zhanglei <1280368848@qq.com>
2018-05-03 11:09:35 +0200

    modification partie graphique

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require 'vendor/autoload.php';
 include_once 'env.php';
 include_once 'global.php';
 include_once 'session.php';
-
+//include_once 'tt/graphe.php';
 include_once 'db.php';
 include_once 'tools.php';
 
