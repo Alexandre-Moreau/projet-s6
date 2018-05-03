ed59c9bc876b06c6db57aa3fa9f5d21f64a72a39
EmericVO <emeric.v.o@gmail.com>
2018-05-03 10:27:16 +0200

    Update backButton.php

--- views/components/backButton.php
+++ views/components/backButton.php
@@ -1,1 +1,1 @@
-<button type="button" class="btn btn-secondary" id="backButton"><?php echo _BACKBUTTON></button>
+<button type="button" class="btn btn-secondary" id="backButton"><?php echo _BACKBUTTON;?></button>
