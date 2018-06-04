517272341e706cf5244b56468b7e60fa0bad3318
JoffreyDuc <joffreyduc@gmail.com>
2018-06-04 23:24:45 +0200

    Responsive design

--- views/header.php
+++ views/header.php
@@ -5,6 +5,9 @@
 
 		<meta charset="UTF-8">
 		
+		<!-- Responsive design -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
 		<link rel="shortcut icon" type="image/png" href="./images/favicon.png"/>
 		<!-- https://www.flaticon.com/packs/space-3 -->
 	
